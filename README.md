a9d99d6732fece7bc77c61176119072ac165804b
binge <johannake40@gmail.com>
2023-03-10 20:25:40 -0500

    enum

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # platformer
 
 Keeping things simple, basic 2d platformer in the style of Super Mario Bros. and Shovel Knight
+
+# LAB 6
+added a scene selector script
---
+++ platformer/Assets/Scripts/sceneSelector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class sceneSelector : MonoBehaviour
+{
+    public SceneSelectorEnum sceneSelect;
+    Button btn;
+
+    private void Awake()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(delegate { LoadSceneScene(sceneSelect); });
+    }
+
+    public enum SceneSelectorEnum
+    {
+        Old = 0,
+        New = 1,
+    }
+
+    public void LoadSceneScene(SceneSelectorEnum scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+}
